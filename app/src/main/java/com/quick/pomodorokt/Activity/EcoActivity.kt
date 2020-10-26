1407d5ead556e2120924c93bb07c3e3ffbb4c511
Violin <rizki_violin_radhiyan@git.com>
2020-10-26 15:12:16 +0700

    add eco mode

---
+++ app/src/main/java/com/quick/pomodorokt/Activity/EcoActivity.kt
@@ -0,0 +1,11 @@
+package com.quick.pomodorokt.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EcoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eco)
+    }
+}
---
+++ app/src/main/java/com/quick/pomodorokt/Notif/NotificationHelper.kt
@@ -0,0 +1,4 @@
+package com.quick.pomodorokt.Notif
+
+class NotificationHelper {
+}
