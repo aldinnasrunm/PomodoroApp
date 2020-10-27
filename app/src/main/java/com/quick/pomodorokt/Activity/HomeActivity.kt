e4e2007c64afee080998c9c681e3fed9a8898ee6
Violin <rizki_violin_radhiyan@git.com>
2020-10-27 12:32:22 +0700

    redesign and add lottie animation

--- app/src/main/java/com/quick/pomodorokt/Activity/HomeActivity.kt
+++ app/src/main/java/com/quick/pomodorokt/Activity/HomeActivity.kt
@@ -1,23 +1,36 @@
 package com.quick.pomodorokt.Activity
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import com.quick.pomodorokt.R
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var cardPomo : CardView
+    lateinit var cardEco : CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(1)
         setContentView(R.layout.activity_home)
         supportActionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.setStatusBarColor(Color.TRANSPARENT)
 
-        btn_pomo.setOnClickListener {
+        cardPomo = findViewById(R.id.cardPomo)
+        cardEco = findViewById(R.id.cardEco)
+
+        cardPomo.setOnClickListener {
             startActivity(Intent(this@HomeActivity, PomoActivity::class.java))
             Toast.makeText(this, "Pomodoro selected", Toast.LENGTH_SHORT).show()
         }
-        btn_eco.setOnClickListener {
+        cardEco.setOnClickListener {
             startActivity(Intent(this@HomeActivity, EcoLandingActivity::class.java))
             Toast.makeText(this, "Eco selected", Toast.LENGTH_SHORT).show()
 
